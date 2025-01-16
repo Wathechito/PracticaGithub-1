eb25a7e12e36f8f088cb3e08e9dc526a3afc8341
Agenciaaon <alwaysonperu@gmail.com>
2025-01-15 22:11:45 -0500

    v2 con input

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 # desafio: calcular la edad de una persona
 # primer desafio consiste en no usar input()
-# rama main
+# rama main_2 act.
 
-año_actual = 2025
-año_nacimiento = 1994
-condicion = True # True si la persona ya cumplio años, False si no los ha cumplido
+año_actual = int(input('Ingresa el año actual: '))
+print("\n")
+
+año_nacimiento = int(input('Ingrese tu año de nacimiento: '))
+print("\n")
+
+# True si la persona ya cumplio años, False si no los ha cumplido
+años_cumplidos = input('¿Ya cumplistes años? (si/no): ')
+print("\n")
+
+if años_cumplidos == 'si':
+    años_cumplidos = True
+else:
+    años_cumplidos = False
 
 edad = año_actual - año_nacimiento
 
-if condicion == False:
-    print(f"La edad de la persona es: {edad} años")
+if años_cumplidos == True:
+    print(f"Tu edad es: {edad} años")
 else:
-    print(f"La edad de la persona es: {edad - 1} años")
+    print(f"Tu edad es: {edad - 1} años")
